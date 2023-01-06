8129ec1831043d13fb7929a9db1786eb494dab53
krithi2895 <krithi.2895@gmail.com>
2023-01-06 07:31:35 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,128 @@
+html
+{
+  box-sizing: border-box;  
+}
+body
+{
+  padding:20px;
+}
+h2,h4,p
+{
+  margin:0;
+}
+
+*,*:before,*:after
+{
+  box-sizing :inherit;
+}
+#outer-div
+{
+  width:600px;
+  height: 400px;
+  
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  
+}
+#top-div
+{
+  width:100%;
+  height:45%;
+  padding : 25px;
+
+}
+
+#bottom-div
+{
+  width: 100%;
+  height:55%;
+  color:white;
+
+}
+
+#bottom-left-div
+{
+  width:50%;
+  height: 100%;
+  padding: 25px;
+  display:inline-block;
+  vertical-align:top;
+  background-color:#20B2AA
+}
+
+#bottom-right-div
+{
+  display:inline-block;
+  vertical-align:top;
+  width:50%;
+  height: 100%;
+  padding:25px;
+  background-color: #22BFB7;
+}
+
+#top-div h2
+{
+  color: #20B2AA;
+ margin-bottom:15px;
+}
+#top-div h4
+{
+ margin-bottom:10px;
+  color: #c0df34;
+}
+#top-div p
+{
+  color:#98a6bd;
+}
+
+button
+{
+  color: white;
+  background-color:#c0df34;
+  border: none;
+  width:100%;
+  padding:10px;
+  border-radius: 5px;
+}
+#price{
+  font-weight:700;
+  font-size: 1.3rem;
+  display:inline-block;
+}
+ #bottom-left-div h4
+{
+  margin-bottom: 20px;
+}
+#pricerate
+{
+  display: inline-block;
+  postion: relative;
+  top: -5px;
+  margin-left: 5px;
+}
+
+#price-container
+{
+  margin-bottom:10px;
+}
+
+#bottom-left-div p
+{
+  margin-bottom: 40px;
+  font-size: 0.8rem;
+}
+
+ul{
+  list-style:none;
+  padding:0;
+  opacity:0.8;
+  font-size:0.8rem;
+}
+button:hover
+{
+  cursor:pointer;
+  background-color:#A6B317;
+  transform:scale(1.02);
+}
